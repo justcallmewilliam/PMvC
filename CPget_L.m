99b1ed5243cb377bd8a0b69bd251487302ea2830
William_Guo <32009061+justcallmewilliam@users.noreply.github.com>
2022-08-29 09:17:53 +0800

    Delete CPget_L.m

--- CPget_L.m
+++
@@ -1,9 +0,0 @@
-function [L] = CPget_L(A,K)
-    numOfSamples = size(A, 1);
-    L = zeros(size(A));
-    W = zeros(size(A));
-    W = W + A;
-    DN = diag( 1./sqrt(sum(A, 2)+eps) );
-    LapN = speye(numOfSamples) - DN * A * DN;
-    L = L + LapN;
-end
